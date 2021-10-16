fd571949e7de3ea1f888bbe4a67ae54c64beb9f7
OziRis78 <oguzhantg3@gmail.com>
2021-10-16 21:31:50 +0300

    v0.0.10

--- build/docs/javadoc/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"telek.fx.main"}]
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module telek.fx.main {
-    requires javafx.controls;
-    requires javafx.web;
-    requires javafx.swing;
-    requires javafx.media;
-
-    exports com.telek.telekfx;
-}
